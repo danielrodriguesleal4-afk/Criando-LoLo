43600f4e17a79b0d51f205e823ea021b7b5778e5
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-14 18:53:56 +0000

    Soma númerica

---
+++ MAT.java
@@ -0,0 +1,8 @@
+public class MAT {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+        int resultado = numero1 + numero2;
+        System.out.println("A soma de 10 + 20 é: " + resultado);
+    }
+}
