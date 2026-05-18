245eb0e62bf48d7b9f2e4bcdda6b83d2581f0c67
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-18 18:52:27 +0000

    Finalizado com sucesso!!!

--- boss.java
+++ boss.java
@@ -7,15 +7,15 @@ public class boss {
     System.out.print("Qual é o seu nome? :" );
     String name = leitor.nextLine();
     System.out.println("Boa tarde! " + name + ", Sejá bem vindo.");
-    System.out.println("Quantos anos vc tem?");
+    System.out.print("Quantos anos vc tem? ");
     int n1 = leitor.nextInt();
-    System.out.println("Qual é a sua altura?");
+    System.out.print("Qual é a sua altura? ");
     double n2 = leitor.nextDouble();
     System.out.println("=========== Cadastro finalizado =========");
     System.out.println("=========== Informações do Cadastro =======");
-    System.out.println("Seu nome é: " + name + ".");
-    System.out.println("Vc tem:" + n1 + " anos.");
-    System.out.println("É sua altura é:" + ".");
+    System.out.println("Seu nome é : " + name + ".");
+    System.out.println("Vc tem: " + n1 + " anos.");
+    System.out.println("É sua altura é: " + n2 + " metros.");
         }
     }
 }
