5f25e860b2b8edf37a9e36dd935321fda2c9f6dc
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-18 18:22:35 +0000

    Andamento do cadastro.

--- voz.java
+++ voz.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class voz {
     public static void main(String[] args) {
         try (Scanner leitor = new Scanner(System.in)) {
+    System.out.println("=========== Cadastro ============");
     System.out.print("Qual é o seu nome? :" );
     String name = leitor.nextLine();
-    System.out.println("Seja bem vindo " + name );
+    System.out.println("Boa tarde! " + name + ", Sejá bem vindo.");
+    System.out.println("");
         }
     }
 }
