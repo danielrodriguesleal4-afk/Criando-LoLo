b58ff2888221a688ebdfbdabc38b31d3996f1694
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-18 17:35:44 +0000

    Saudação básica.

---
+++ voz.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class voz {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+    System.out.print("Qual é o seu nome? :" );
+    String name = leitor.nextLine();
+    System.out.print("Seja bem vindo " + name );
+        leitor.close();
+    }
+}
