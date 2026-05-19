7b807e5a6828655219747fd92fbd9628ecdea4b8
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-19 17:13:56 +0000

    Desafio N1 finalizado.

--- MAT.java
+++ MAT.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class MAT {
     public static void main(String[] args) {
         try (Scanner leitor = new Scanner(System.in)) {
-        System.out.print("Escolha o primeiro de 1 a 100: ");
+        System.out.print("Escolha um número maior que 50: ");
         int n1 = leitor.nextInt();
-        System.out.print("Escolha o segundo número de 1 a 100: ");
+        System.out.print("Escolha um segundo número menor que 50: ");
         int n2 = leitor.nextInt();
-        int resultado = n1 + n2;
-        System.out.print("Aqui está a soma: " + resultado);
+        int resultado = n1 - n2;
+        System.out.println("Aqui está a subtração: " + resultado);
         }
     }
 }
