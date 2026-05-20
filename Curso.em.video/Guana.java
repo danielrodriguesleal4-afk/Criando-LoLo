2a1367de8b947e619639fc4d56082bcf40ec9bb9
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-20 17:02:07 +0000

    Relogio v 0.1

---
+++ Curso.em.video/Guana.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Guana {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+    System.out.println("Agora são...");
+    System.out.println(relogio.toString());
+    }
+}
