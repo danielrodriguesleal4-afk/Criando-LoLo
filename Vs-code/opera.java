2e50d08b56b81ec7ff6f8a32839f76d57a6d8106
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-26 10:24:54 +0000

    Save

--- Vs-code/opera.java
+++ Vs-code/opera.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class opera {
    public static void main(String[] args) {
       try (Scanner leitor = new Scanner(System.in)) {
-   
+   System.out.print("");
        }
     }
 }
