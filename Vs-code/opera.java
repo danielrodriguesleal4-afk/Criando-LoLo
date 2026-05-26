88cd9bc4401bb7887e9f5354946188eb39dc577a
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-26 10:39:04 +0000

    Pre seleção

--- Vs-code/opera.java
+++ Vs-code/opera.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class opera {
    public static void main(String[] args) {
       try (Scanner leitor = new Scanner(System.in)) {
-   System.out.print("");
+   System.out.println("Escolha uma letra entre A,B,C até J.");
+   System.out.println("(Letras maiúsculas)");
+   
        }
     }
 }
