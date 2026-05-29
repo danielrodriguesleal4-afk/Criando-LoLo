fe323b25011869666896004f05e5a8d7b3be2f5f
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-29 01:19:45 +0000

    Erro

--- Vs-code/erro.java
+++ Vs-code/erro.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class erro {
     public static void main(String[] args) {
-        
+try (Scanner leitor = new Scanner(System.in)) {
+    System.out.println("===== Escolhas de valor =====");
+    System.out.print("Escolha uma letra de A,B,C,D,E,F,G,H,I é J: ");
+String escolha = leitor.next();
+    if (escolha == A) {
+        System.out.println("");
+    }
+}
     }
 }
