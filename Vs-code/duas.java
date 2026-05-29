a33887855accaceda68e143b572c9af7da655948
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-05-29 19:34:12 +0000

    Previa

--- Vs-code/duas.java
+++ Vs-code/duas.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class duas {
     public static void main(String[] args) {
 try (Scanner leitor = new Scanner(System.in)) {
-    
+    System.out.println("===== Frase motivacional =====");
+    System.out.print("Escolha uma letra de A até J: ");
+    String escolha = leitor.next();
+    if (escolha.equals("A")) {
+        System.out.println("BOMMMMMMMMMMMMM");
     }
+}
 }
