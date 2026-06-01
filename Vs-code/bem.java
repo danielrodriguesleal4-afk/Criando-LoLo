a4d73034c092e2c9cc2360a2880b61d87b3b9284
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-06-01 14:55:30 +0000

    aaaaaaa

--- Vs-code/bem.java
+++ Vs-code/bem.java
@@ -3,7 +3,19 @@ import java.util.Scanner;
 public class bem {
     static public void main(String[] args) {
 try (Scanner leitor =new Scanner(System.in)) {
-
-}
+    System.out.println("===== Nota da prova. =====");
+    System.out.print("Qual foi a sua nota?: ");
+        String escolha = leitor.next();
+    if (escolha.equalsIgnoreCase("a")) {
+        System.out.println("");
+        System.out.println("Vc foi exelente. Parabéns");
+    }
+    else if (escolha.equalsIgnoreCase("b")) {
+        System.out.println("Vc foi bem. Continue melhorando.");
+    }
+    else if (escolha.equalsIgnoreCase("c")) {
+        System.out.println("Hummmm... Preocupante.");
+    }
+        }
     }
 }
