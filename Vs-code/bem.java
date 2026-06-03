41d2ec79b33f611c9a15185a06301e1829bbd492
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-06-03 17:32:57 +0000

    Questão if e else Okk

--- Vs-code/bem.java
+++ Vs-code/bem.java
@@ -7,15 +7,29 @@ try (Scanner leitor =new Scanner(System.in)) {
     System.out.print("Qual foi a sua nota?: ");
         String escolha = leitor.next();
     if (escolha.equalsIgnoreCase("a")) {
-        System.out.println("");
         System.out.println("Vc foi exelente. Parabéns");
     }
     else if (escolha.equalsIgnoreCase("b")) {
-        System.out.println("Vc foi bem. Continue melhorando.");
+        System.out.println("Ótimo!. Continue melhorando.");
     }
     else if (escolha.equalsIgnoreCase("c")) {
-        System.out.println("Hummmm... Preocupante.");
+        System.out.println("Bom. Bom desempenho.");
     }
-        }
+    else if (escolha.equalsIgnoreCase("d")) {
+        System.out.println("Regular. Vc precisa melhorar.");
+    }
+    else if (escolha.equalsIgnoreCase("e")) {
+        System.out.println("Suficiente. Aprovado, mas precisa melhorar.");
+    }
+    else if (escolha.equalsIgnoreCase("f")) {
+        System.out.println("Insuficiente. Estude mais.");
+    }
+    else if(escolha.equalsIgnoreCase("g") || escolha.equalsIgnoreCase("j")) {
+        System.out.println("Crítico. Atenção!!! Vc precisa de ajuda.");
+    }
+    else {
+        System.out.println("Opção inválida. Por favor, escolha uma letra de A a J.");
+    }
+}
     }
 }
