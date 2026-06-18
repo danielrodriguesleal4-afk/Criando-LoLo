9c435e75f70c08a005e619304adf5b20b5126872
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-06-18 15:06:25 +0000

    aaaaaaa

---
+++ Vs-code/pratica.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pratica {
+    public static void main(String[] args) {
+        try (Scanner leitor = new Scanner(System.in)){
+    System.out.println("===== Listas =====");
+    System.out.println("Se vc deseja multiplicação, coloque N 1");
+    System.out.println("Se deseja soma, N 2");
+    System.out.println("É se for Divisão, N 3");
+      String escolha = leitor.next();
+    switch (escolha) {
+        case 1 
+    }
+        }
+    }
+}
