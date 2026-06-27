dade18b867e6547f5fb7e9b22aeaa35363c15883
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-06-27 13:52:23 +0000

    Comentario.

--- Vs-code/pratica.java
+++ Vs-code/pratica.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class pratica {
     public static void main(String[] args) {
         try (Scanner leitor = new Scanner(System.in)){
-    System.out.println("===== Listas =====");
+            // Menu de opções do sistema.
+    System.out.println("===== Calculadora =====");
     System.out.println("Se vc deseja multiplicação, coloque N 1");
     System.out.println("Se deseja soma, N 2");
     System.out.println("É se for Divisão, N 3");
     System.out.print("Digite aqui: ");
       String escolha = leitor.next();
     switch (escolha) {
+
+       // Tabela de multiplicação
         case "1":
             System.out.print("Me fale o primeiro valor: ");
             int n1 = leitor.nextInt();
@@ -18,14 +21,18 @@ public class pratica {
             int n3 = n1 * n2;
             System.out.println("Aqui está o resultado: " + n3);
     break;
+
+        // Tabela de soma
         case "2":
             System.out.print("Me fale um valor inteiro: ");
             int n4 = leitor.nextInt();
             System.out.print("Um segundo valor: ");
             int n5 = leitor.nextInt();
             int n6 = n4 + n5;
-            System.out.print("Aqui está a soma: " + n6);
+            System.out.println("Aqui está o resultado: " + n6);
     break;
+
+        // Tabela de divisão
         case "3":
             System.out.print("Me fale um valor: ");
             double n7 = leitor.nextDouble();
