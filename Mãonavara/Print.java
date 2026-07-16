01472274fe138abcfbf4d86f2a9e54e4a344b9d5
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-07-16 01:46:31 +0000

    Ensinamentos.

---
+++ Mãonavara/Print.java
@@ -0,0 +1,11 @@
+public class Print {
+    public static void main(String[] args) { 
+    float nota = 8.5f;
+        // Comando "print" com "f" de formatado.
+    System.out.printf("A nota é %.2f \n", nota);
+        /* Ó "%" representa casas decimais.
+         * É os numeros após o ponto, e quantas casas deseja mostrar.
+         * É por ultimo o comando "\n" e para pular uma linha.
+         */
+    }
+}
