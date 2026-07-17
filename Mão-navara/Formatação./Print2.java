92a2f7fcb6922b6c155965e61268364dbca82722
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-07-17 01:46:07 +0000

    v1

--- Mão-navara/Formatação./Print2.java
+++ Mão-navara/Formatação./Print2.java
@@ -1,9 +1,16 @@
+// importando o Scanner.
 import java.util.Scanner;
 
 public class Print2 {
     public static void main(String[] args) {
+        /* "leitor" e a criação da variavel.
+         * 
+         * 
+         */
         try (Scanner leitor = new Scanner(System.in)) {
-            
+    String nome = leitor.nextLine();
+    float nota = leitor.nextFloat();
+        System.out.printf("A nota de %s é %.2", nome, nota);
         }
     }
 }
