ea771d50d1985f0337b2091d60dc9f8b9abe9e94
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-07-21 02:41:20 +0000

    Converção finalizada.

---
+++ Mão-navara/Formatação./Converção.java
@@ -0,0 +1,18 @@
+public class Converção {
+    public static void main(String[] args) {
+    // Metodo "toString" para converter em palavra.
+        int idade = 30;
+        String valor = Integer.toString(idade);
+    // Metodo "parseInt" para converter em número.
+    // Para "int" o comando necéssario é "%d".
+        String valor2 = "30";
+        int idade2 = Integer.parseInt(valor);
+    // Metodo "parseFloat" para números reais. 
+        String valor3 = "30.5";
+        float idade3 = Float.parseFloat(valor3);
+    // Mostrando os resultados.
+        System.out.printf("Sua idade é: %s \n", idade);
+        System.out.printf("Sua idade é: %d \n", idade2);
+        System.out.printf("Sua idade é: %.3f \n", idade3);
+    }
+}
--- Mão-navara/Formatação./Tipos.java
+++
@@ -1,6 +0,0 @@
-public class Tipos {
-    public static void main(String[] args) {
-        float nota = (float) 8.5;
-        System.out.println("A nota do sistema é: " + nota);
-    }
-}
