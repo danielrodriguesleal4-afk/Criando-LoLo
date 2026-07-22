20bc65cad221c82d5b256135db3e976c8389587d
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-07-22 02:34:48 +0000

    Concertos

--- Mão-navara/Formatação./Converção.java
+++ Mão-navara/Formatação./Converção.java
@@ -6,12 +6,12 @@ public class Converção {
     // Metodo "parseInt" para converter em número.
     // Para "int" o comando necéssario é "%d".
         String valor2 = "30";
-        int idade2 = Integer.parseInt(valor);
+        int idade2 = Integer.parseInt(valor2);
     // Metodo "parseFloat" para números reais. 
         String valor3 = "30.5";
         float idade3 = Float.parseFloat(valor3);
     // Mostrando os resultados.
-        System.out.printf("Sua idade é: %s \n", idade);
+        System.out.printf("Sua idade é: %s \n", valor);
         System.out.printf("Sua idade é: %d \n", idade2);
         System.out.printf("Sua idade é: %.3f \n", idade3);
     }
