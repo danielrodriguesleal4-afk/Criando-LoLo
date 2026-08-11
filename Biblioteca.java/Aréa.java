abc000866c45390664766b744826d58adc4df8b3
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-08-11 02:58:03 +0000

    Quiz Concluido.

---
+++ Biblioteca.java/Aréa.java
@@ -0,0 +1,8 @@
+public class Aréa {
+    public static void main(String[] args) {
+        int x = 8, y = 3;
+        int w = x / y;
+        String z = (w % 2 == 0) ? "Frase1":"Frase2";
+        System.out.println(z);
+    }
+}
---
+++ Mão-navara/Exercicios./quiz.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class quiz {
+    public static void main(String[] args) {
+
+    // --- leitor. --- //
+    try (Scanner leitor = new Scanner(System.in)) {
+
+    System.out.println("Tente advinhar o número que estou pensando de 1 a 5.");
+    System.out.print("Digite o número: ");
+    int N = leitor.nextInt();
+
+    double A = Math.random();
+    int X = (int) (1 + A * (5 - 1));
+
+    String R = (N==X) ? "Arcertou":"Errou, estava pensando em " + X;
+
+    System.out.println(R);
+
+        }
+    }
+}
