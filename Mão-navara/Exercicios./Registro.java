6d05666964c527302d17405a8ea5fd3fa9274312
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-08-13 21:53:27 +0000

    Teste 1

---
+++ Mão-navara/Exercicios./Registro.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Registro {
+    public static void main(String[] args) {
+        try (Scanner leitor = new Scanner(System.in)) {
+
+    // Anúncio 1
+    System.out.println("--- Registro. ---");
+
+    // Nome.
+    System.out.print("Seu nome: ");
+    String nome = leitor.nextLine();
+
+    // Idade.
+    System.out.print("Sua idade: ");
+    int Ida = leitor.nextInt();
+
+    // Nota.
+    System.out.print("Sua nota: ");
+    double nota = leitor.nextDouble();
+
+    //Sistema.
+    String R = (Ida>=18) ? nome + " Sua nota é: " + nota:"Vc não tem idade suficiente.";
+
+    // Resultado.
+    System.out.println(R);
+
+        }
+    }
+}
