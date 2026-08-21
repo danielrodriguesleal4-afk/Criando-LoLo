437ab37e7196e4cace85a24550ccc044376ddcf6
danielrodriguesleal4-afk <danielrodriguesleal4@gmail.com>
2026-08-21 02:13:13 +0000

    Calculadora com switch finalizado.

--- Mão-navara/Multiplas/Cal.java
+++ Mão-navara/Multiplas/Cal.java
@@ -4,8 +4,10 @@ public class Cal {
     public static void main(String[] args) {
         try (Scanner leitor = new Scanner(System.in)) {
 
-    // Escolhas.
+    // Anuncio: 1.
     System.out.println(" --- Calculadora personalizada. --- ");
+
+    // Escolhas das opções.
     System.out.println("Digite 1 para soma.");
     System.out.println("Digite 2 para subtração.");
     System.out.println("Digite 3 para mutiplicação.");
@@ -19,28 +21,92 @@ public class Cal {
 
         case 1: // --- Soma. --- // 
 
+        // Anuncio: Soma.
             System.out.println(" --- Soma escolhida. --- ");
+
+        // Escolhas de valores.
             System.out.print("Digite o primeiro valor: ");
-            int so1 = leitor.nextInt();
+                int so1 = leitor.nextInt();
             System.out.print("Digite o segundo valor: ");
-            int so2 = leitor.nextInt();
-            int R1 = so1 + so2;
+                int so2 = leitor.nextInt();
+
+           // Calculo.
+                int R1 = so1 + so2;
+
+        // Resultado.
             System.out.println(" --- Resultado. --- ");
             System.out.println("A soma deu: " + R1);
 
+        // Fim:
+            break;
+
+
         case 2: // --- Subtração. --- // 
 
-            System.err.println(" --- Subtração. --- ");
+        // Anuncio: Subt.
+            System.err.println(" --- Subtração Escolhida. --- ");
+
+        // Escolhas de valores.
+            System.out.print("Digite o primeiro valor: ");
+                int div1 = leitor.nextInt();
+            System.out.print("Digite o segundo valor: ");
+                int div2 = leitor.nextInt();
+
+            // Calculo.
+                int R2 = div1 - div2;
+
+        // Resultado da subtração.
+            System.out.println(" --- Resultado. --- ");
+            System.out.println("A subtração deu: " + R2);
+
+        // Fim
+            break; 
+
 
         case 3: // --- Multiplicação. --- //
 
-            System.out.println(" --- Multiplicação. --- ");
+        // Anuncio: Mult.
+            System.out.println(" --- Multiplicação Escolhido. --- ");
+
+        // Escolhas de valores.
+            System.out.print("Digite o primeiro número: ");
+                int Mul1 = leitor.nextInt();
+            System.out.print("Digite o segundo número: "); 
+                int Mul2 = leitor.nextInt();
+                
+            // Calculo.
+                int R3 = Mul1 * Mul2;
+            
+        // Resultado.
+            System.out.println(" --- Resultado. --- ");
+            System.out.println("A multiplicação deu: " + R3);
+
+        // Fim 
+            break;
+
 
         case 4: // --- Divição. --- //
 
-            System.out.println(" --- Divição. --- ");
-    }
+        //Anuncio: Divi.
+            System.out.println(" --- Divição Escolhido. --- ");
 
+        // Escolhas de valores. 
+            System.out.print("Digite o primeiro número: ");
+                double Div1 = leitor.nextDouble();
+            System.out.print("Digite o segundo número: ");
+                double Div2 = leitor.nextDouble();
+                
+            // Calculo.
+                double R4 = Div1 / Div2;
+
+        // Resultado
+            System.out.println(" --- Resultado. --- ");
+            System.out.println("A divição deu: " + R4);
+ 
+        // Fim 
+            break;
+
+            }
         }
     }
 }
